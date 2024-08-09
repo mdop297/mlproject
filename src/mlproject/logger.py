3b24f3897a553ac55c8fb05b63d3abb9cbe3eb28
mdop729 <lenhatminh12321@gmail.com>
2024-08-09 03:45:58 +0000

    add logging and exceptions

---
+++ src/mlproject/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os, sys
+from datetime import datetime
+
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,"running_logs.log")
+
+logging.basicConfig(
+    format="[ %(asctime)s ] line number: %(lineno)d; module: %(name)s - %(levelname)s: %(module)s: %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ]
+
+)
+
+makelog = logging.getLogger("mlproject")
+
